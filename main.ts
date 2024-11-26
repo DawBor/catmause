c0564f1eb5fb00bc8c18f7554d6f8ec40f40718e
DawBor <db.chojnacki@gmail.com>
2024-11-26 17:44:50 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,13 @@
-let ghost: game.LedSprite = null
-let licznik = 0
-let speed = 0
-let wynik = 0
 let pacman: game.LedSprite = null
+let wynik = 0
+let speed = 0
+let licznik = 0
+let ghost: game.LedSprite = null
+basic.showNumber(3)
+basic.showNumber(2)
+basic.showNumber(1)
+basic.clearScreen()
+basic.showString("GO")
 basic.forever(function () {
     ghost = game.createSprite(4, 4)
     licznik = 0
@@ -29,6 +34,7 @@ basic.forever(function () {
             speed = 300
         }
         if (ghost.isTouching(pacman)) {
+            music.play(music.builtinPlayableSoundEffect(soundExpression.yawn), music.PlaybackMode.UntilDone)
             game.gameOver()
         }
         licznik += 1
@@ -39,13 +45,13 @@ basic.forever(function () {
     pacman = game.createSprite(2, 2)
     while (true) {
         basic.pause(100)
-        if (input.rotation(Rotation.Pitch) < -10) {
+        if (input.rotation(Rotation.Pitch) < -5) {
             pacman.change(LedSpriteProperty.Y, -1)
-        } else if (input.rotation(Rotation.Pitch) > 10) {
+        } else if (input.rotation(Rotation.Pitch) > 5) {
             pacman.change(LedSpriteProperty.Y, 1)
-        } else if (input.rotation(Rotation.Roll) < -10) {
+        } else if (input.rotation(Rotation.Roll) < -5) {
             pacman.change(LedSpriteProperty.X, -1)
-        } else if (input.rotation(Rotation.Roll) > 10) {
+        } else if (input.rotation(Rotation.Roll) > 5) {
             pacman.change(LedSpriteProperty.X, 1)
         }
     }
